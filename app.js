7a85460b62ff096b7ed4b3292223d7052b88bdfd
jppuerta1 <jppuertaorrego@gmail.com>
2020-11-10 19:23:38 -0800

    segundo commit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const express = require('express')
 const app= express ()
-app.get('/',(req,res)=>res.send('HcUy6Re2LLBRtj'))
+app.get('/',(req,res)=>res.send('<h1>HcUy6Re2LLBRtj</h1>'))
 app.listen(3000,()=>console.log('HcUy6Re2LLBRtj'))
